871a45f03af5a4ab353bccba303a3a2d237d1940
Alexey Karnachev <alekseykarnachev@gmail.com>
2025-04-17 18:44:51 +0300

    ui: nodes preview

--- shaderbox/main.py
+++ shaderbox/main.py
@@ -60,8 +60,25 @@ def main():
     imgui.create_context()
     imgui_renderer = GlfwRenderer(window)
 
-    nodes = [Node()]
-    selected_node = nodes[0]
+    nodes = [Node() for _ in range(10)]
+    current_node = nodes[0]
+
+    def create_new_node():
+        nonlocal current_node
+
+        node = Node()
+        nodes.append(node)
+
+        current_node = node
+
+    def delete_current_node():
+        nonlocal current_node
+
+        if len(nodes) == 1:
+            return
+
+        nodes.remove(current_node)
+        current_node = nodes[-1]
 
     while not glfw.window_should_close(window):
         window_width, window_height = glfw.get_window_size(window)
@@ -78,18 +95,51 @@ def main():
         gl.screen.use()
         gl.clear()
 
-        imgui.set_next_window_size(window_width, window_height)
-        imgui.set_next_window_position(0, 0)
+        # ----------------------------------------------------------------
+        # Handle hotkeys
+        io = imgui.get_io()
+        if io.key_ctrl and imgui.is_key_pressed(ord("N")):
+            create_new_node()
+
+        if io.key_ctrl and imgui.is_key_pressed(ord("D")):
+            delete_current_node()
+
+        # ----------------------------------------------------------------
+        # Main menu bar
+        main_menu_height = 0
+
+        if imgui.begin_main_menu_bar().opened:
+
+            # ------------------------------------------------------------
+            # Node menu
+            if imgui.begin_menu("Node", True).opened:
+
+                if imgui.menu_item("New", "Ctrl+N", False, True)[1]:
+                    create_new_node()
+
+                if imgui.menu_item("Delete Current", "Ctrl+D", False, True)[1]:
+                    delete_current_node()
+
+                imgui.end_menu()  # Node
+
+            main_menu_height = imgui.get_item_rect_size()[1]
+            imgui.end_main_menu_bar()  # main menu
+
+        # ----------------------------------------------------------------
+        # Main window
+        imgui.set_next_window_size(window_width, window_height - main_menu_height)
+        imgui.set_next_window_position(0, main_menu_height)
         imgui.begin(
             "ShaderBox",
             flags=imgui.WINDOW_NO_COLLAPSE
-            | imgui.WINDOW_NO_COLLAPSE
-            | imgui.WINDOW_NO_DECORATION
-            | imgui.WINDOW_ALWAYS_AUTO_RESIZE,
+            | imgui.WINDOW_ALWAYS_AUTO_RESIZE
+            | imgui.WINDOW_NO_TITLE_BAR,
         )
 
+        control_panel_min_height = 600
+
         # ----------------------------------------------------------------
-        control_panel_min_height = 300
+        # current node image
         min_image_height = 100
 
         max_image_height = max(
@@ -97,12 +147,12 @@ def main():
             imgui.get_content_region_available()[1] - control_panel_min_height,
         )
         max_image_width = imgui.get_content_region_available_width()
-        image_aspect = np.divide(*selected_node.texture.size)
+        image_aspect = np.divide(*current_node.texture.size)
         image_width = min(max_image_width, max_image_height * image_aspect)
         image_height = min(max_image_height, max_image_width / image_aspect)
 
         imgui.image(
-            selected_node.texture.glo,
+            current_node.texture.glo,
             width=image_width,
             height=image_height,
             uv0=(0, 1),
@@ -110,40 +160,68 @@ def main():
         )
 
         # ----------------------------------------------------------------
+        # Control panel
         region_width, region_height = imgui.get_content_region_available()
         control_panel_height = max(control_panel_min_height, region_height)
         control_panel_width = region_width
-        imgui.begin_child(
+        with imgui.begin_child(
             "control_panel",
             width=control_panel_width,
             height=control_panel_height,
             border=False,
-        )
+        ):
 
-        # ----------------------------------------------------------------
-        node_previews_width, node_previews_height = imgui.get_content_region_available()
-        node_previews_width = int(node_previews_width / 3.0)
-        imgui.begin_child(
-            "node_previews",
-            width=node_previews_width,
-            height=node_previews_height,
-            border=True,
-        )
-        imgui.end_child()  # node_previews
+            # ------------------------------------------------------------
+            # Node previews
+            node_preview_width, node_preview_height = (
+                imgui.get_content_region_available()
+            )
+            node_preview_width = node_preview_width / 3.0
+            with imgui.begin_child(
+                "node_previews",
+                width=node_preview_width,
+                height=node_preview_height,
+                border=True,
+            ):
+                preview_size = 130
+                n_cols = imgui.get_content_region_available_width() // (
+                    preview_size + 5
+                )
+                n_cols = max(1, n_cols)
+                for i, node in enumerate(nodes):
+                    with imgui.begin_child(
+                        f"node_preview_{i}",
+                        width=preview_size,
+                        height=preview_size,
+                    ):
+                        s = (preview_size - 10) / max(node.texture.size)
+                        image_width = node.texture.size[0] * s
+                        image_height = node.texture.size[1] * s
 
-        # ----------------------------------------------------------------
-        imgui.same_line()
-        uniforms_width, uniforms_height = imgui.get_content_region_available()
-        imgui.begin_child(
-            "uniforms",
-            width=uniforms_width,
-            height=uniforms_height,
-            border=True,
-        )
-        imgui.end_child()  # uniforms
+                        imgui.image_button(
+                            node.texture.glo,
+                            width=image_width,
+                            height=image_height,
+                            uv0=(0, 1),
+                            uv1=(1, 0),
+                        )
 
-        # ----------------------------------------------------------------
-        imgui.end_child()  # control_panel
+                    if (i + 1) % n_cols != 0:
+                        imgui.same_line()
+                    else:
+                        imgui.spacing()
+
+            # ------------------------------------------------------------
+            # Uniforms
+            imgui.same_line()
+            uniforms_width, uniforms_height = imgui.get_content_region_available()
+            with imgui.begin_child(
+                "uniforms",
+                width=uniforms_width,
+                height=uniforms_height,
+                border=True,
+            ):
+                pass
 
         # ----------------------------------------------------------------
         imgui.end()  # ShaderBox
