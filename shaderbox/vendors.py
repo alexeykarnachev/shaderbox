9fdb73706dea7b19059b9df4705550648aca9d70
Alexey Karnachev <alekseykarnachev@gmail.com>
2025-04-25 15:53:25 +0300

    compress vendors

--- shaderbox/vendors.py
+++ shaderbox/vendors.py
@@ -5,13 +5,18 @@ import httpx
 from PIL import Image
 
 
-def get_modelbox_depthmap(image: Image.Image) -> Image.Image:
-    url = "http://localhost:8228/infer_depth_pro"
-
+def _image_to_base64(image: Image.Image) -> str:
     buffer = BytesIO()
     image.save(buffer, format="PNG")
-    base64_image = base64.b64encode(buffer.getvalue()).decode("utf-8")
+    return base64.b64encode(buffer.getvalue()).decode("utf-8")
 
+
+def _base64_to_image(base64_str: str) -> Image.Image:
+    image_data = base64.b64decode(base64_str)
+    return Image.open(BytesIO(image_data))
+
+
+def _post_image_to_endpoint(url: str, base64_image: str) -> str:
     with httpx.Client() as client:
         response = client.post(
             url,
@@ -23,35 +28,18 @@ def get_modelbox_depthmap(image: Image.Image) -> Image.Image:
             timeout=30.0,
         )
         response.raise_for_status()
+    return str(response.json()["image"])
 
-    depthmap_base64 = response.json()["image"]
-    depthmap_image_data = base64.b64decode(depthmap_base64)
-    depthmap_image = Image.open(BytesIO(depthmap_image_data))
 
-    return depthmap_image
+def get_modelbox_depthmap(image: Image.Image) -> Image.Image:
+    url = "http://localhost:8228/infer_depth_pro"
+    base64_image = _image_to_base64(image)
+    depthmap_base64 = _post_image_to_endpoint(url, base64_image)
+    return _base64_to_image(depthmap_base64)
 
 
 def get_modelbox_bg_removal(image: Image.Image) -> Image.Image:
     url = "http://localhost:8228/infer_bg_removal"
-
-    buffer = BytesIO()
-    image.save(buffer, format="PNG")
-    base64_image = base64.b64encode(buffer.getvalue()).decode("utf-8")
-
-    with httpx.Client() as client:
-        response = client.post(
-            url,
-            headers={
-                "accept": "application/json",
-                "Content-Type": "application/json",
-            },
-            json={"image": base64_image},
-            timeout=30.0,
-        )
-        response.raise_for_status()
-
-    no_bg_base64 = response.json()["image"]
-    no_bg_image_data = base64.b64decode(no_bg_base64)
-    no_bg_image = Image.open(BytesIO(no_bg_image_data))
-
-    return no_bg_image
+    base64_image = _image_to_base64(image)
+    no_bg_base64 = _post_image_to_endpoint(url, base64_image)
+    return _base64_to_image(no_bg_base64)
