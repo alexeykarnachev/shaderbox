5552b1f57f217c5aff7c04aab93b1ad3c1ba0adc
Alexey Karnachev <alekseykarnachev@gmail.com>
2025-06-17 02:22:24 +0300

    change modelbox api

--- shaderbox/modelbox.py
+++ shaderbox/modelbox.py
@@ -19,10 +19,10 @@ def _post_file(file_path: Path | str) -> str:
     return str(response.json())
 
 
-def _get_file(file_id: str, output_path: Path) -> None:
+def _get_file(file_name: str, output_path: Path) -> None:
     with httpx.Client() as client:
         response = client.get(
-            f"{settings.modelbox_file_url}/{file_id}",
+            f"{settings.modelbox_file_url}/{file_name}",
             timeout=10.0,
         )
         response.raise_for_status()
@@ -45,22 +45,22 @@ T = TypeVar("T", Image, Video)
 
 
 def infer_media_model(media: T, model_name: str, output_dir: Path) -> T:
-    input_file_id = _post_file(media.details.file_details.path)
+    input_file_name = _post_file(media.details.file_details.path)
 
     with httpx.Client() as client:
         response = client.post(
             settings.modelbox_media_model_url,
             json={
-                "file_id": input_file_id,
+                "file_name": input_file_name,
                 "model_name": model_name,
             },
             timeout=600.0,
         )
         response.raise_for_status()
 
-    output_file_id = response.json()["file_id"]
-    output_file_path = Path(output_dir / output_file_id)
-    _get_file(output_file_id, output_file_path)
+    output_file_name = response.json()["file_name"]
+    output_file_path = Path(output_dir / output_file_name)
+    _get_file(output_file_name, output_file_path)
 
     output_media = media.__class__(output_file_path)
     return output_media
