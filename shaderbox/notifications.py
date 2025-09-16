5f1e4468641b139a51f1bec89544d58c58cb2e44
Alexey Karnachev <alekseykarnachev@gmail.com>
2025-09-16 09:59:55 +0300

    factor out notifications

---
+++ shaderbox/notifications.py
@@ -0,0 +1,43 @@
+from collections import deque
+
+import imgui
+
+
+class Notifications:
+    def __init__(self, stack_size: int = 5) -> None:
+        self._stack = deque(maxlen=stack_size)  # type: ignore
+
+    def push(self, text: str, color=(0.0, 1.0, 0.0), ttl=5.0) -> None:  # type: ignore
+        self._stack.appendleft([text, color, ttl])
+
+    def update_and_draw(self) -> None:
+        # ----------------------------------------------------------------
+        # Update
+        alive_inds = []
+        for i in range(len(self._stack)):
+            self._stack[i][2] -= imgui.get_io().delta_time
+            if self._stack[i][2] > 0.0:
+                alive_inds.append(i)
+
+        if len(alive_inds) != len(self._stack):
+            self._stack = deque(
+                [self._stack[i] for i in alive_inds], maxlen=self._stack.maxlen
+            )
+
+        if not self._stack:
+            return
+
+        # ----------------------------------------------------------------
+        # Draw
+        pad = 10.0
+        gap = 7.0
+
+        window_size = imgui.get_window_size()
+        current_y = pad
+
+        for text, color, _ in self._stack:
+            text_size = imgui.calc_text_size(text)
+            x = window_size.x - text_size.x - pad
+            imgui.set_cursor_pos((x, current_y))  # type: ignore
+            imgui.text_colored(text, *color)
+            current_y += text_size.y + gap
--- shaderbox/ui.py
+++ shaderbox/ui.py
@@ -7,7 +7,6 @@ import shutil
 import subprocess
 import time
 import webbrowser
-from collections import deque
 from collections.abc import Callable, Iterable, Sequence
 from pathlib import Path
 from typing import Any, Self, TypeVar
@@ -42,6 +41,7 @@ from shaderbox.media import (
     ResolutionDetails,
     Video,
 )
+from shaderbox.notifications import Notifications
 from shaderbox.ui_models import (
     UIAppState,
     UIMessage,
@@ -237,46 +237,6 @@ def load_nodes_from_dir(root_dir: Path) -> dict[str, UINode]:
             ui_nodes[node_dir.name] = load_node_from_dir(node_dir)
 
     return ui_nodes
-
-
-class Notifications:
-    def __init__(self, stack_size: int = 5) -> None:
-        self._stack = deque(maxlen=stack_size)  # type: ignore
-
-    def push(self, text: str, color=(0.0, 1.0, 0.0), ttl=5.0) -> None:  # type: ignore
-        self._stack.appendleft([text, color, ttl])
-
-    def update_and_draw(self) -> None:
-        # ----------------------------------------------------------------
-        # Update
-        alive_inds = []
-        for i in range(len(self._stack)):
-            self._stack[i][2] -= imgui.get_io().delta_time
-            if self._stack[i][2] > 0.0:
-                alive_inds.append(i)
-
-        if len(alive_inds) != len(self._stack):
-            self._stack = deque(
-                [self._stack[i] for i in alive_inds], maxlen=self._stack.maxlen
-            )
-
-        if not self._stack:
-            return
-
-        # ----------------------------------------------------------------
-        # Draw
-        pad = 10.0
-        gap = 7.0
-
-        window_size = imgui.get_window_size()
-        current_y = pad
-
-        for text, color, _ in self._stack:
-            text_size = imgui.calc_text_size(text)
-            x = window_size.x - text_size.x - pad
-            imgui.set_cursor_pos((x, current_y))  # type: ignore
-            imgui.text_colored(text, *color)
-            current_y += text_size.y + gap
 
 
 class App:
